bc794afe955d63784309a787c14c43c29e8e9160
Paul <sampavel2012@yandex.ru>
2022-02-17 20:12:02 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input Grille size");
+        int num = in.nextInt();
+        System.out.println("Your Input");
+        System.out.println(num);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
